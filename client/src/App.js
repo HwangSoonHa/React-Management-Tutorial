97f5a10af0aea7eadc8db228b559f77b4288441c
soonha <tnsgk5105@recordline.co.kr>
2020-07-16 22:51:02 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -21,34 +21,24 @@ const styles = theme => ({
   }
 });
 
-const customers = [
-  {
-    id:1,
-    image:'https://placeimg.com/64/64/any',
-    name:'홍길동',
-    birthday:'920202',
-    gender:'남자',
-    job:'대학생'
-  },
-  {
-    id:2,
-    image:'https://placeimg.com/64/64/any',
-    name:'서예지',
-    birthday:'985874',
-    gender:'여자',
-    job:'프로그래머'
-  },
-  {
-    id:3,
-    image:'https://placeimg.com/64/64/any',
-    name:'유재석',
-    birthday:'201010',
-    gender:'남자',
-    job:'의사'
-  }
-]
-
 class App extends Component {
+
+  state = {
+    customers : ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers:res})) //실행이 될 경우
+      .catch(err => console.log(err)) //오류가 날 경우
+  }
+
+  callApi = async()=>{
+    const response = await fetch('api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render(){
     const {classes} = this.props;
 
@@ -66,7 +56,7 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => {
+            {this.state.customers ? this.state.customers.map(c => {
               return (
                 <Customer 
                   key = {c.id}
@@ -78,7 +68,7 @@ class App extends Component {
                   job={c.job}
                 />
               );
-            })}
+            }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message:"Hello Express!"});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            id:1,
+            image:'https://placeimg.com/64/64/any',
+            name:'홍길동',
+            birthday:'920202',
+            gender:'남자',
+            job:'대학생'
+          },
+          {
+            id:2,
+            image:'https://placeimg.com/64/64/any',
+            name:'서예지',
+            birthday:'985874',
+            gender:'여자',
+            job:'프로그래머'
+          },
+          {
+            id:3,
+            image:'https://placeimg.com/64/64/any',
+            name:'유재석',
+            birthday:'201010',
+            gender:'남자',
+            job:'의사'
+          }
+    ]);
 });
 
 app.listen(port, ()=>console.log(`Listening on port ${port}`));
